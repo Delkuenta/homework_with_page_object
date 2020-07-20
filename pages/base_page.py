dda5cc415fada58af318291a6c243a0b3f7d8d1c
Sergey Khabarov <delkuenta@gmail.com>
2020-07-20 17:01:50 +0300

    Выполнена подготовка к код ревью 1. Сгруппировал тесты: сначала для код ревью и остальные 2. Уменьшил не явное ожидание до 4 секунд на BasePage 3. Удалил личные пояснительные комментарии

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,7 +8,7 @@ from .locators import BasePageLocators
 
 
 class BasePage:
-    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout) # неявное ожидание
